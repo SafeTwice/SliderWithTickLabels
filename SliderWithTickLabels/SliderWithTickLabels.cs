8880b0506b733b203ba6b7af05cc6a5c33dce55f
Jesús González <gonzalez.jesus@safetwice.com>
2022-11-09 13:59:30 +0100

    Add properties to set tick labels explicitly or calculated by frequency, independently of the tick "marks"

--- SliderWithTickLabels/SliderWithTickLabels.cs
+++ SliderWithTickLabels/SliderWithTickLabels.cs
@@ -1,65 +1,74 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SliderWithTickLabels
 {
-    /// <summary>
-    /// Siga os passos 1a ou 1b e depois 2 para usar esse controle personalizado em um arquivo XAML.
-    ///
-    /// Passo 1a) Usando o controle personalizado em um arquivo XAML que já existe no projeto atual.
-    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
-    /// deve ser usado:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels"
-    ///
-    ///
-    /// Passo 1b) Usando o controle personalizado em um arquivo XAML que existe em um projeto diferente.
-    /// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
-    /// deve ser usado:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels;assembly=SliderWithTickLabels"
-    ///
-    /// Também será necessário adicionar nesse projeto uma referência ao projeto que contém esse arquivo XAML
-    /// e Recompilar para evitar erros de compilação:
-    ///
-    ///     No Gerenciador de Soluções, clique com o botão direito no projeto alvo e
-    ///     "Adicionar Referência"->"Projetos"->[Selecione esse projeto]
-    ///
-    ///
-    /// Passo 2)
-    /// Vá em frente e use seu controle no arquivo XAML.
-    ///
-    ///     <MyNamespace:CustomControl1/>
-    ///
-    /// </summary>
-    public class SliderWithTickLabels : Slider
-    {
-		public static readonly DependencyProperty GeneratedTicksProperty;
+	/// <summary>
+	/// Siga os passos 1a ou 1b e depois 2 para usar esse controle personalizado em um arquivo XAML.
+	///
+	/// Passo 1a) Usando o controle personalizado em um arquivo XAML que já existe no projeto atual.
+	/// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+	/// deve ser usado:
+	///
+	///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels"
+	///
+	///
+	/// Passo 1b) Usando o controle personalizado em um arquivo XAML que existe em um projeto diferente.
+	/// Adicione o atributo XmlNamespace ao elemento raiz do arquivo de marcação onde ele 
+	/// deve ser usado:
+	///
+	///     xmlns:MyNamespace="clr-namespace:SliderWithTickLabels;assembly=SliderWithTickLabels"
+	///
+	/// Também será necessário adicionar nesse projeto uma referência ao projeto que contém esse arquivo XAML
+	/// e Recompilar para evitar erros de compilação:
+	///
+	///     No Gerenciador de Soluções, clique com o botão direito no projeto alvo e
+	///     "Adicionar Referência"->"Projetos"->[Selecione esse projeto]
+	///
+	///
+	/// Passo 2)
+	/// Vá em frente e use seu controle no arquivo XAML.
+	///
+	///     <MyNamespace:CustomControl1/>
+	///
+	/// </summary>
+	public class SliderWithTickLabels : Slider
+	{
+		private static readonly DependencyPropertyKey InternalTickLabelsPropertyKey;
+		
+		public static readonly DependencyProperty TickLabelsProperty;
 		public static readonly DependencyProperty TickLabelTemplateProperty;
+		public static readonly DependencyProperty TickLabelFrequencyProperty;
 
-		[Bindable(true)]
-		public DoubleCollection GeneratedTicks
+		[Bindable(false)]
+		[Browsable(false)]
+		public DoubleCollection InternalTickLabels
 		{
 			get
 			{
-				return base.GetValue(SliderWithTickLabels.GeneratedTicksProperty) as DoubleCollection;
+				return (DoubleCollection) base.GetValue(SliderWithTickLabels.InternalTickLabelsPropertyKey.DependencyProperty);
+			}
+			private set
+			{
+				base.SetValue(SliderWithTickLabels.InternalTickLabelsPropertyKey, value);
+			}
+		}
+
+		[Bindable(true)]
+		[Category("Appearance")]
+		public DoubleCollection TickLabels
+		{
+			get
+			{
+				return (DoubleCollection) base.GetValue(SliderWithTickLabels.TickLabelsProperty) ;
 			}
 			set
 			{
-				base.SetValue(SliderWithTickLabels.GeneratedTicksProperty, value);
+				base.SetValue(SliderWithTickLabels.TickLabelsProperty, value);
 			}
 		}
 
@@ -68,7 +77,7 @@ namespace SliderWithTickLabels
 		{
 			get
 			{
-				return base.GetValue(SliderWithTickLabels.TickLabelTemplateProperty) as DataTemplate;
+				return (DataTemplate) base.GetValue(SliderWithTickLabels.TickLabelTemplateProperty) ;
 			}
 			set
 			{
@@ -76,23 +85,51 @@ namespace SliderWithTickLabels
 			}
 		}
 
+		[Bindable(true)]
+		[Category("Appearance")]
+		public double TickLabelFrequency
+		{
+			get
+			{
+				return (double) base.GetValue(SliderWithTickLabels.TickLabelFrequencyProperty);
+			}
+			set
+			{
+				base.SetValue(SliderWithTickLabels.TickLabelFrequencyProperty, value);
+			}
+		}
+
 		static SliderWithTickLabels()
-        {
-			SliderWithTickLabels.GeneratedTicksProperty = DependencyProperty.Register("GeneratedTicks", typeof(DoubleCollection), typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
-			SliderWithTickLabels.TickLabelTemplateProperty = DependencyProperty.Register("TickLabelTemplate", typeof(DataTemplate), typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+		{
+			SliderWithTickLabels.InternalTickLabelsPropertyKey = DependencyProperty.RegisterReadOnly("InternalTickLabels", typeof(DoubleCollection), typeof(SliderWithTickLabels),
+				new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+			SliderWithTickLabels.TickLabelsProperty = DependencyProperty.Register("TickLabels", typeof(DoubleCollection), typeof(SliderWithTickLabels),
+				new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender));
+			SliderWithTickLabels.TickLabelTemplateProperty = DependencyProperty.Register("TickLabelTemplate", typeof(DataTemplate), typeof(SliderWithTickLabels),
+				new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+			SliderWithTickLabels.TickLabelFrequencyProperty = DependencyProperty.Register("TickLabelFrequency", typeof(double), typeof(SliderWithTickLabels),
+				new FrameworkPropertyMetadata(double.NaN, FrameworkPropertyMetadataOptions.AffectsRender ));
 
 			FrameworkElement.DefaultStyleKeyProperty.OverrideMetadata(typeof(SliderWithTickLabels), new FrameworkPropertyMetadata(typeof(SliderWithTickLabels)));
-        }
+		}
 
 		protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
 		{
 			base.OnPropertyChanged(e);
 
-			var propertyNames = new string[] { "Minimum", "Maximum", "TickFrequency", "Ticks", "IsDirectionReversed" };
+			var propertyNames = new string[]
+			{
+				nameof(Minimum),
+				nameof(Maximum),
+				nameof(TickFrequency),
+				nameof(TickLabelFrequency),
+				nameof(Ticks),
+				nameof(IsDirectionReversed)
+			};
 
 			if (IsInitialized && propertyNames.Contains(e.Property.Name))
 			{
-				CalculateTicks();
+				CalculateTickLabels();
 			}
 		}
 
@@ -100,7 +137,7 @@ namespace SliderWithTickLabels
 		{
 			base.OnInitialized(e);
 
-			CalculateTicks();
+			CalculateTickLabels();
 		}
 
 		protected override Geometry GetLayoutClip(Size layoutSlotSize)
@@ -108,27 +145,47 @@ namespace SliderWithTickLabels
 			return ClipToBounds ? base.GetLayoutClip(layoutSlotSize) : null;
 		}
 
-		private void CalculateTicks()
+		private void CalculateTickLabels()
 		{
-			if (this.Ticks != null && this.Ticks.Any())
+			if (this.TickLabels != null && this.TickLabels.Any())
 			{
-				this.GeneratedTicks = new DoubleCollection(this.Ticks.Union(new double[] { this.Minimum, this.Maximum }).Where(t => this.Minimum <= t && t <= this.Maximum));
+				this.InternalTickLabels = new DoubleCollection(this.TickLabels.Union(new double[] { this.Minimum, this.Maximum }).Where(t => this.Minimum <= t && t <= this.Maximum));
 			}
 			else
 			{
-				if (this.TickFrequency > 0.0)
+				double tickLabelFrequency = this.TickLabelFrequency;
+				bool isValidTickLabelFrequency = IsValidDoubleValue(tickLabelFrequency);
+
+				if (!isValidTickLabelFrequency && this.Ticks != null && this.Ticks.Any())
 				{
-					this.GeneratedTicks = new DoubleCollection(
-						Enumerable.Range(
-							0,
-							Convert.ToInt32(
-								Math.Ceiling((this.Maximum - this.Minimum) / this.TickFrequency)
-							) + 1
-						)
-						.Select(t => Math.Min(t * this.TickFrequency + this.Minimum, this.Maximum))
-					);
+					this.InternalTickLabels = new DoubleCollection(this.Ticks.Union(new double[] { this.Minimum, this.Maximum }).Where(t => this.Minimum <= t && t <= this.Maximum ));
+				}
+				else
+				{
+					if (!isValidTickLabelFrequency)
+					{
+						tickLabelFrequency = this.TickFrequency;
+					}
+
+					if (tickLabelFrequency > 0.0)
+					{
+						this.InternalTickLabels = new DoubleCollection(
+							Enumerable.Range(
+								0,
+								Convert.ToInt32(
+									Math.Ceiling((this.Maximum - this.Minimum) / tickLabelFrequency)
+								) + 1
+							)
+							.Select(t => Math.Min(t * tickLabelFrequency + this.Minimum, this.Maximum))
+						);
+					}
 				}
 			}
 		}
-    }
+		private static bool IsValidDoubleValue(object value)
+		{
+			double num = (double) value;
+			return !double.IsNaN(num) && !double.IsInfinity(num);
+		}
+	}
 }
